97d317de6562a6cb9666185927cdcddd2d3119b6
Gopal Anand <gopal.anand@sap.com>
2019-09-24 10:53:27 +0530

    intial commit with main index

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# pushnotifications
-nodejs push notifications using google firebase 
+# Push Notifications
+node.js push notifications using google firebase 
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const express = require("express");
+const port = process.env.port || 3000;
+const app = express();
+
+app.listen(port,()=>{console.log(`App is running at port ${port}`)});
